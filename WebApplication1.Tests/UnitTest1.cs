04b8fb5996b8d1f0692d5843fd753b208f68805a
Ibram Reda <abramreda815@gmail.com>
2021-05-04 07:33:37 +0200

    adding unit test

---
+++ WebApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace WebApplication1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // dammy unit test for prove of consept 
+            // to see how containuse integration work on github
+            // with unit test in pipline test
+            Assert.True(1 == 1);
+        }
+    }
+}
